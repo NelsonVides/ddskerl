2c82eb9dbeae11418e0f768b834cc27f57abaa99
Nelson Vides <nelson.vides@erlang-solutions.com>
2025-02-26 20:31:16 +0100

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+ddskerl
+=====
+
+An OTP library
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/ddskerl.erl
@@ -0,0 +1,11 @@
+-module(ddskerl).
+-if(?OTP_RELEASE >= 27).
+-define(MODULEDOC(Str), -moduledoc(Str)).
+-define(DOC(Str), -doc(Str)).
+-else.
+-define(MODULEDOC(Str), -compile([])).
+-define(DOC(Str), -compile([])).
+-endif.
+?MODULEDOC(false).
+
+-export([]).
